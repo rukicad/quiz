1116b47a1eb4d201bc1e69998d61673cb2a1ce86
Rukiye Çadırcı <rukiyecadirci4@gmail.com>
2022-07-25 09:37:30 +0300

    quiz
    Change saved.

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
@@ -22,7 +10,11 @@ Route::middleware([
     config('jetstream.auth_session'),
     'verified'
 ])->group(function () {
-    Route::get('/dashboard', function () {
+    Route::get('/panel', function () {
         return view('dashboard');
     })->name('dashboard');
+});
+
+Route::group(['middleware' => ['auth','isAdmin'],'prefix'=>'admin'], function(){
+
 });
